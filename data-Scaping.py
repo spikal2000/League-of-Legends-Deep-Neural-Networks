2b20485635ac20497f00591be94159ad6fc9a9c0
spikal2000 <spikal1997@gmail.com>
2023-03-01 17:14:59 +0200

    lsts say its working till now

--- data-Scaping.py
+++ data-Scaping.py
@@ -24,27 +24,18 @@ import pandas as pd
 
 
 # Replace YOUR_API_KEY with your actual API key
-API_KEY = "RGAPI-811ced25-f787-4dd3-af75-087bf0129612"
-# Replace REGION with the region you want to query (e.g. "na1", "euw1", etc.)
-REGION = "eun1"
-# Replace TIER with the tier you want to query (e.g. "GOLD")
-TIER = "GOLD"
-
-PAGES = [1]
-
-# Step 1: Get a list of all leagues in the region
-#leagues_url = f"https://{REGION}.api.riotgames.com/lol/league/v4/leagues?api_key={API_KEY}"
-# random_players_url = f"https://eun1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{TIER}/I?page={pages[0]}&api_key={API_KEY}"
-
+API_KEY = "RGAPI-f7bd360b-0bcc-48ea-8244-a2e81c52ae71"
+lol_watcher = LolWatcher(API_KEY)
+TIERS = ["GOLD"]
+RANKS = ["I"]
+REGIONS = ["eun1"]
+PAGES = [1, 2]
 
 
 #Gets random players from all the ranks and returns a list 
 #NOte: we dont have to use this, get_puuids() is ussing this for us
 def get_players():
-    TIERS = ["IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND"]
-    RANKS = ["I", "II", "III", "IV"]
-    REGIONS = ["eun1", "na1"]
-    PAGES = [1]
+    print('geting the players')
     #TIERS = ["GOLD"]
     #RANKS = ["I"]
     #REGIONS = ["eun1"]
@@ -64,47 +55,44 @@ def get_players():
                         print(f"Error: {e}. Retrying in 5 seconds...")
                         time.sleep(5)
                         continue
-                
+    # with open('players.pickle', 'wb') as f:
+    #     pickle.dump(result, f)
     return result
 
 
 def get_puuids():
+    print("geting the puuids")
     players = get_players()
-    
+    players  = [val for sublist in players for val in sublist]
+    # with open('players.pickle', 'rb') as f:
+        # players = pickle.load(f)
     puuids = []
-    for page in range(0, len(PAGES)):
-        for player in players[page]:
-            player_puuid_url = f"https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player['summonerName']}?api_key={API_KEY}"
-            try:
-                player_puuid_response = requests.get(player_puuid_url)
-                if player_puuid_response.status_code == 404:
-                    print("Player not found.")
-                    continue
-                elif player_puuid_response.status_code == 429:
-                    print("Rate limit exceeded. Waiting for 2 min...")
-                    time.sleep(130)
-                    player_puuid_response = requests.get(player_puuid_url)
-                    player_puuid = player_puuid_response.json()
-                    puuid = player_puuid['puuid']
-                    puuids.append(puuid)
-                else:
-                    player_puuid = player_puuid_response.json()
-                    puuid = player_puuid['puuid']
-                    puuids.append(puuid)
-                    time.sleep(5)
-            except requests.exceptions.RequestException as e:
-                print(f"Error: {e}. Retrying in 5 seconds...")
-                time.sleep(5)
+    for player in players:
+        player_puuid_url = f"https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player['summonerName']}?api_key={API_KEY}"
+        try:
+            player_puuid_response = requests.get(player_puuid_url)
+            if player_puuid_response.status_code == 404:
+                print("Player not found.")
                 continue
-            
+            elif player_puuid_response.status_code == 429:
+                print("Rate limit exceeded. Waiting for 2 min...")
+                time.sleep(130)
+                player_puuid_response = requests.get(player_puuid_url)
+                player_puuid = player_puuid_response.json()
+                puuid = player_puuid['puuid']
+                puuids.append(puuid)
+            else:
+                player_puuid = player_puuid_response.json()
+                puuid = player_puuid['puuid']
+                puuids.append(puuid)
+                time.sleep(5)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}. Retrying in 5 seconds...")
+            time.sleep(5)
+            continue
+        
     return puuids
             
-            
-
-
-
-    
-
 
 def get_match_ids():
     puuids = get_puuids()
@@ -116,40 +104,28 @@ def get_match_ids():
     #puuids = ["xKkamFA9ysY6dCEvcTCSkAeCl2I0LFqLDhMxvSPJS3q-JRXa64rc5UlFJ9mV338APusxaJodwh3UoQ", "Iuk2x32ujlq_Bpr_JTEHm7gIDd_TlwYNh4XLZyWdP3PTn-L4heP8u0EUmqVQyB54Efcaym8zAnSIew"]
     
     number_of_matches = 1
-    player_matchId = {}
+    matches = []
     for puuid in puuids:
         # GET A PLAYERS IDS MATCH HISTRY USING PUUI...
         #https://developer.riotgames.com/apis#match-v5/GET_getMatchIdsByPUUID
-        match_ids = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={number_of_matches}&api_key={API_KEY}"
-        # match_ids = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?type=ranked&start=0&count={number_of_matches}&api_key={API_KEY}"
+        # match_ids = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={number_of_matches}&api_key={API_KEY}"
+         #420 means its the ranked solo duo
         try:
-            match_response = requests.get(match_ids)
-            if match_response.status_code == 404:
-                print("match not found.")
-                continue
-            elif match_response.status_code == 429:
-                print("Rate limit exceeded. Waiting for 2 min...")
-                time.sleep(130)
-                match_response = requests.get(match_ids)
-                matches_ids = match_response.json()
-                player_matchId[puuid] = matches_ids
-            else:
-                matches_ids = match_response.json()
-                player_matchId[puuid] = matches_ids
-                time.sleep(5)
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}. Retrying in 5 seconds...")
-            time.sleep(5)
+            match_ids = lol_watcher.match.matchlist_by_puuid(region=REGIONS[0], puuid=puuid, queue=420)
+            matches.append(match_ids)
+            time.sleep(2)
+        except ApiError as err:
+            # Handle any errors that occur
+            print(f"API error: {err}")
+            time.sleep(130)
             continue
+    matches  = [val for sublist in matches for val in sublist]
     
-    
-    return player_matchId
+    return matches
 
 
 # Define the columns for the DataFrame
 columns = ['match_id',
-           'red_Player1', 'red_Player2', 'red_Player3', 'red_Player4', 'red_Player5',
-           'Blue_player1', 'Blue_player2', 'Blue_player3', 'Blue_player4', 'Blue_player5',
            'average_kills-red_Player1', 'average_kills-red_Player2', 'average_kills-red_Player3', 'average_kills-red_Player4', 'average_kills-red_Player5',
            'average_kills-Blue_player1', 'average_kills-Blue_player2', 'average_kills-Blue_player3', 'average_kills-Blue_player4', 'average_kills-Blue_player5',
            'blueTeam_win']
@@ -157,136 +133,80 @@ columns = ['match_id',
 # Define an empty DataFrame with the columns
 df = pd.DataFrame(columns=columns)
 
-def get_game_info():
+def run():
     
     match_ids = get_match_ids()
-    #match_ids = {'xKkamFA9ysY6dCEvcTCSkAeCl2I0LFqLDhMxvSPJS3q-JRXa64rc5UlFJ9mV338APusxaJodwh3UoQ': ['EUN1_3321814596']}
-    
-    
-    
-    
-    for player in match_ids:
+    for match in match_ids:
         win = 0
         blueTeam = []
         redTeam = []
-        for match_id in match_ids[player]:
-            win = 0
-            blueTeam = []
-            redTeam = []
-            game_info_url = f"https://europe.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={API_KEY}"
-            try:
-                game_responce = requests.get(game_info_url)
-                if game_responce.status_code == 404:
-                    print("match not found.")
-                    continue
-                if game_responce.status_code == 429:
-                    print("Rate limit exceeded. Waiting for 2 min...")
-                    time.sleep(130)
-                time.sleep(3)
-                game_responce = requests.get(game_info_url)
-                game_info = game_responce.json()  
-                #play here:
-                participants = game_info['metadata']['participants'] #gives a list of 10 participants[ 0-9]
-                pinfo = get_participants_info(participants)
-
-                for participant in participants:
-                    for i in range(0,10):
-                        if participant == game_info['info']['participants'][i]['puuid']:
-                            if game_info['info']['participants'][i]['teamId'] == 100:
-                                blueTeam.append(participant)
-                            if game_info['info']['participants'][i]['teamId'] == 200:
-                                redTeam.append(participant)
-                        
-                pinfo = get_participants_info(participants)
-                for part in range(0,10):
-                    if player == game_info['info']['participants'][part]['puuid']:
-                        if game_info['info']['participants'][part]['win'] == True:
-                            win = 1
-                
-                
-              
-                row = [match_id] + [player for player in redTeam]+ [player for player in blueTeam] + [pinfo[player]['kills'] for player in redTeam] + [pinfo[player]['kills'] for player in blueTeam]  + [win]                       
-                #row = [match_id] + [pinfo[player]['kills'] for player in redTeam] + [pinfo[player]['kills'] for player in blueTeam] + [pinfo[player]['deaths'] for player in redTeam] + [pinfo[player]['deaths'] for player in blueTeam] + [pinfo[player]['assists'] for player in redTeam] + [pinfo[player]['assists'] for player in blueTeam] + [win]                       
-                #apply to df 
-                df.loc[len(df)] = row
-                    
-                        
-                        
-                        
-                    #-----------   
-                    
-                        
-                        
-                        
-                    #----------- 
-                time.sleep(3)
-            except requests.exceptions.RequestException as e:
-                print(f"Error: {e}. Retrying in 5 seconds...")
-                time.sleep(5)
-                continue
-        
-    return 0
-
-
-
-
-
-
-
-
-
-
-
-
+        try:
+            match_details = lol_watcher.match.by_id(region=REGIONS[0],  match_id=match)
+            participants = match_details['metadata']['participants']
+            for participant in participants:
+                for i in range(0,10):
+                    if participant == match_details['info']['participants'][i]['puuid']:
+                        if match_details['info']['participants'][i]['teamId'] == 100:
+                            blueTeam.append(participant)
+                        if match_details['info']['participants'][i]['teamId'] == 200:
+                            redTeam.append(participant)
+            #start here
+            bInfo = get_participants_info(blueTeam)
+            rInfo = get_participants_info(redTeam)
+            #teamID = 100 == blueteam
+            if match_details['info']['teams'][0]['win']:
+                win = 1
+            
+            bl = []
+            rd = []
+            for i in range(0,5):
+                bl.append(bInfo[i]['kills'])
+                rd.append(rInfo[i]['kills'])
+            row = [match] + rd + bl + [win]
+            df.loc[len(df)] = row
+            print(row)
+        except ApiError as err:
+            # Handle any errors that occur
+            print(f"API error: {err}")
+            print(f"Sleeps for 2 min...")
+            time.sleep(130)
+            continue
+            
+    return df
 
 def get_participants_info(participants):
+    print("getting participants info")
     result = {}
-
+    i = 0
     for participant in participants:
-        time.sleep(3)
-        match_history_url = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{participant}/ids?start=0&count=2&api_key={API_KEY}"
-        
+        time.sleep(2)
         try:
-            match_history_response = requests.get(match_history_url)
-            if match_history_response.status_code != 200:
-                print(f'Error: {match_history_response.text}')
-                continue
-            match_history = match_history_response.json()
+            match_history = lol_watcher.match.matchlist_by_puuid(region=REGIONS[0], puuid=participant, queue=420)
             kills = []
             #deaths = []
             #assists = []
             #onMyWayPings = []
             #visionScore = []
             for match_id in match_history:
-                time.sleep(5)
-                game_info_url = f"https://europe.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={API_KEY}"
+                time.sleep(2)
                 try:
-                    game_info_response = requests.get(game_info_url)
-                    if game_info_response.status_code != 200:
-                        print(f'Error: {game_info_response.text}')
-                        continue
-                    if game_info_response.status_code == 429:
-                        print("Rate limit exceeded. Waiting for 2 min...")
-                        time.sleep(130)
-                    match = game_info_response.json()
-                    for participant_identity in match['info']['participants']:
+                    match_details = lol_watcher.match.by_id(region=REGIONS[0],  match_id=match_id)
+                    for participant_identity in match_details['info']['participants']:
                         if participant_identity['puuid'] == participant:
                             kills.append(participant_identity['kills'])
                             #deaths.append(participant_identity['deaths'])
                             #assists.append(participant_identity['assists'])
                             #onMyWayPings.append(participant_identity['onMyWayPings'])
                             #visionScore.append(participant_identity['visionScore'])
-                        
-                                
-                except requests.exceptions.RequestException as e:
-                    print(f"Error: {e}. Retrying in 5 seconds...")
-                    time.sleep(5)
+                except ApiError as err:
+                    print(f"API error: {err}")
                     continue
-            result[participant] = {'kills': average(kills)}
-            #result[participant] = {'kills': average(kills),'deaths':average(deaths), 'assists':average(assists), 'onMyWayPings':average(onMyWayPings), 'visionScore':average(visionScore)}
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}. Retrying in 5 seconds...")
-            time.sleep(5)
+            result[i] = {'kills': average(kills)}
+            i+=1
+        except ApiError as err:
+            # Handle any errors that occur
+            print(f"API error: {err}")
+            time.sleep(130)
             continue
     return result
 
@@ -297,17 +217,4 @@ def average(numbers):
         return 0
     return sum(numbers) / len(numbers)
 
-    
-
-                   
-# game_info['info']['participants'][9]
-
-          
-                
-       
-# # Game_info = f"https://europe.api.riotgames.com/lol/match/v5/matches/{matches_ids[0]}?api_key={API_KEY}"
-# # #https://developer.riotgames.com/apis#match-v5/GET_getMatch
-# game_responce = requests.get(Game_info)
-# game_info = game_responce.json()         
-                
   
